c8ae5b877f8a056303fbf602a0bdc30d5802ad1b
GuoJinYi819 <gjy2000@>
2020-05-18 14:07:09 +0800

    初始化项目

---
+++ app/src/main/java/com/wd/doctor/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wd.doctor.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wd.doctor.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
