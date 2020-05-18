db4ce755e83375e87a813a1e355f63fb2836dd21
GuoJinYi819 <gjy2000@>
2020-05-18 14:45:54 +0800

    跳转登入界面

---
+++ app/src/main/java/com/wd/doctor/ui/activity/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.wd.doctor.ui.activity
+
+import com.wd.doctor.R
+import com.wd.doctor.base.BaseActivity
+
+/**ClassName: Doctor
+ * @author 作者 : GuoJinYi
+ * @version 创建时间：2020/5/18 0018 14:36
+ * @Description: 用途：完成特定功能
+ */
+class LoginActivity:BaseActivity() {
+    override fun initLayoutId(): Int {
+        return R.layout.activity_login
+    }
+
+    override fun initLintener() {
+
+    }
+
+    override fun initData() {
+
+    }
+}
--- app/src/main/java/com/wd/doctor/ui/activity/StartPagerActivity.kt
+++ app/src/main/java/com/wd/doctor/ui/activity/StartPagerActivity.kt
@@ -1,7 +1,11 @@
 package com.wd.doctor.ui.activity
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import androidx.core.animation.addListener
 import com.wd.doctor.R
 import com.wd.doctor.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_start_pager.*
 
 /**ClassName: Doctor
  * @author 作者 : GuoJinYi
@@ -9,15 +13,46 @@ import com.wd.doctor.base.BaseActivity
  * @Description: 用途：完成特定功能
  */
 class StartPagerActivity :BaseActivity(){
+
+    val objectAnimator by lazy { ObjectAnimator.ofFloat(activitybg,"alpha",0f,0.5f,1f) }
+
     override fun initLayoutId(): Int {
         return R.layout.activity_start_pager
     }
 
     override fun initLintener() {
+        //动画监听
+        objectAnimator.addListener(object :Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
 
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                //跳转页面
+                startActivityFinish<LoginActivity>()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+        })
     }
 
     override fun initData() {
+        //动画时间
+        objectAnimator.duration = 2000
+        //启动
+        objectAnimator.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (objectAnimator != null) {
+            objectAnimator.clone()
+        }
 
     }
 }
