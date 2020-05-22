a176e87133ca12eb076ad0a2b5462d7ffe4e039a
GuoJinYi819 <gjy2000@>
2020-05-22 17:50:39 +0800

    我的界面资料展示

--- app/src/main/java/com/wd/doctor/ui/activity/MyActivity.kt
+++ app/src/main/java/com/wd/doctor/ui/activity/MyActivity.kt
@@ -1,7 +1,11 @@
 package com.wd.doctor.ui.activity
 
+import com.bumptech.glide.Glide
 import com.wd.doctor.R
 import com.wd.doctor.base.BaseActivity
+import com.wd.doctor.bean.LoginBean
+import com.wd.doctor.mvp.finddoctorbyId.FindDoctorByIdPresenter
+import com.wd.doctor.mvp.finddoctorbyId.IFindDoctorByIdContract
 import kotlinx.android.synthetic.main.activity_my.*
 
 /**ClassName: Doctor
@@ -9,7 +13,10 @@ import kotlinx.android.synthetic.main.activity_my.*
  * @version 创建时间：2020/5/22 0022 17:21
  * @Description: 用途：完成特定功能
  */
-class MyActivity:BaseActivity() {
+class MyActivity:BaseActivity(), IFindDoctorByIdContract.IView {
+
+    val presenter by lazy { FindDoctorByIdPresenter(this) }
+
     override fun initLayoutId(): Int {
         return R.layout.activity_my
     }
@@ -21,5 +28,17 @@ class MyActivity:BaseActivity() {
     }
 
     override fun initData() {
+        //查询个人信息
+        presenter.getMessage()
+    }
+    //个人信息
+    override fun onFindDoctorSuccess(bean: LoginBean) {
+        val result = bean.result
+        result?.let {
+            Glide.with(this).load(result.imagePic).into(ivImagePic)
+        }
+    }
+
+    override fun onFindDoctorFailed(error: String) {
     }
 }
